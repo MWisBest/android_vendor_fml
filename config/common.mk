301e54349bc5721ff6cf4de441f4eafb56587e87
Kyle Repinski <repinski23@gmail.com>
2016-08-13 03:16:41 -0500

    Cleanup config file.

--- config/common.mk
+++ config/common.mk
@@ -1,43 +1,42 @@
 ## Common FML Config ##
 
-# Superuser
-SUPERUSER_EMBEDDED := true
-SUPERUSER_PACKAGE_PREFIX := com.android.settings.fml.superuser
+# overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/fml/overlay/common
 
-PRODUCT_PACKAGES += \
-	Superuser \
-	su
-
-PRODUCT_PROPERTY_OVERRIDES += \
-	persist.sys.root_access=3
-
-# Boot animation
-PRODUCT_COPY_FILES += \
-	vendor/fml/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
 
 # FML init.rc
 PRODUCT_COPY_FILES += \
 	vendor/fml/prebuilt/common/etc/init.fml.rc:root/init.fml.rc
 
-ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.com.google.clientidbase=android-google
-else
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
-endif
+
+# Boot animation
+PRODUCT_COPY_FILES += \
+	vendor/fml/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+
+
+# General props
+PRODUCT_GMS_CLIENTID_BASE ?= android-google
 
 PRODUCT_PROPERTY_OVERRIDES += \
+	ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE) \
 	keyguard.no_require_sim=true \
 	ro.com.android.dataroaming=false \
 	ro.com.android.wifi-watchlist=GoogleGuest \
 	ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
 	ro.setupwizard.enterprise_mode=1
 
+
+# General packages
+PRODUCT_PACKAGES += \
+	CellBroadcastReceiver \
+	Launcher3
+
+
 # init.d support
 PRODUCT_COPY_FILES += \
 	vendor/fml/prebuilt/common/bin/sysinit:system/bin/sysinit \
 	vendor/fml/prebuilt/common/etc/init.d/00fml:system/etc/init.d/00fml
+
 
 # xposed support
 ifeq ($(USE_XPOSED_FRAMEWORK),true)
@@ -48,15 +47,3 @@ PRODUCT_PACKAGES += \
 	XposedBridge \
 	libxposed_art
 endif
-
-PRODUCT_PACKAGES += \
-	CellBroadcastReceiver \
-	Launcher3
-
-# Chromium Prebuilt
-ifeq ($(PRODUCT_PREBUILT_WEBVIEWCHROMIUM),yes)
--include prebuilts/chromium/$(TARGET_DEVICE)/chromium_prebuilt.mk
-endif
-
-# overlays
-PRODUCT_PACKAGE_OVERLAYS += vendor/fml/overlay/common
